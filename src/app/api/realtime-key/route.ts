ec171c44bab32ca9799ddc2ec6b2aa3ae0178d66
burnout909 <znlwmdhkd1@yonsei.ac.kr>
2026-01-16 21:13:36 +0900

    edit transcribe model

--- src/app/api/realtime-key/route.ts
+++ src/app/api/realtime-key/route.ts
@@ -21,7 +21,7 @@ export async function GET() {
                         input: {
                             transcription: {
                                 language: "ko",
-                                "model": "gpt-4o-transcribe"
+                                "model": "whisper-1"
                             }
                         },
                     },
